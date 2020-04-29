c536383cf45f62e3cc600d786133f0470dc96894
emcoo-49 <liyatao@emcoo.com>
2020-04-29 19:03:08 +0800

    添加死信队列

--- rabbitmq/src/main/java/com/example/rabbitmq/broadcast/MQBroadCast.java
+++ rabbitmq/src/main/java/com/example/rabbitmq/broadcast/MQBroadCast.java
@@ -4,6 +4,8 @@ import org.springframework.amqp.core.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.HashMap;
+
 /**
  * @AUTHOR yatao.li
  * @TIME 2020-04-09 11:45
@@ -16,25 +18,51 @@ public class MQBroadCast {
     //队列
     public static final String MQ_QUEUE = "MQ_QUEUE";
 
+    //交换机
+    public static final String DLX_MQ_EXCHANGE = "DLX_MQ_EXCHANGE";
+
+    //队列
+    public static final String DLX_MQ_QUEUE = "DLX_MQ_QUEUE";
+
     //路由键
     public static final String MQ_ROUTING_KEY = "MQ_ROUTING_KEY";
 
     //创建交换机
     @Bean
-    public Exchange getAttendanceMachineLogExchange() {
+    public Exchange getMQExchange() {
         return ExchangeBuilder.directExchange(MQ_EXCHANGE).durable(true).build();
+    }
+
+    //创建死信交换机
+    @Bean
+    public FanoutExchange getDLXMQExchange() {
+        return ExchangeBuilder.fanoutExchange(DLX_MQ_EXCHANGE).durable(true).build();
     }
 
     //创建队列
     @Bean
-    public Queue getAttendanceMachineQueue() {
-        return new Queue(MQ_QUEUE,true);
+    public Queue getMQQueue() {
+        HashMap<String, Object> args = new HashMap<>();
+        args.put("x-dead-letter-exchange", DLX_MQ_EXCHANGE);
+        return new Queue(MQ_QUEUE, true, false, false, args);
+    }
+
+    //创建死信队列
+    @Bean
+    public Queue getDLXMQQueue() {
+        return new Queue(DLX_MQ_QUEUE, true);
     }
 
     //交换机和队列绑定
     @Bean
-    public Binding bindAttendanceMachine() {
-        return BindingBuilder.bind(getAttendanceMachineQueue()).to(getAttendanceMachineLogExchange()).with(MQ_ROUTING_KEY).noargs();
+    public Binding bindMQ() {
+        return BindingBuilder.bind(getMQQueue()).to(getMQExchange()).with(MQ_ROUTING_KEY).noargs();
+    }
+
+    //死信交换机和队列绑定
+    @Bean
+    public Binding bindDLXMQ() {
+        return BindingBuilder.bind(getDLXMQQueue()).to(getDLXMQExchange());
     }
 
 }
--- rabbitmq/src/main/java/com/example/rabbitmq/lisner/MQLisner.java
+++ rabbitmq/src/main/java/com/example/rabbitmq/lisner/MQLisner.java
@@ -5,12 +5,16 @@ import com.example.rabbitmq.service.IRabbitMessageService;
 import com.rabbitmq.client.Channel;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.amqp.RabbitProperties;
+import org.springframework.boot.autoconfigure.amqp.RabbitRetryTemplateCustomizer;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
 
@@ -34,34 +38,53 @@ public class MQLisner {
     @Autowired
     private IRabbitMessageService rabbitMessageService;
 
+    @Transactional
     @RabbitListener(queues = MQBroadCast.MQ_QUEUE)
     public void monitoringMethod(Channel channel, Message message) throws IOException {
+        //执行数据
+        data(channel, message, "mq");
+    }
+
+    @Transactional
+    @RabbitListener(queues = MQBroadCast.DLX_MQ_QUEUE)
+    public void dlx(Channel channel, Message message) throws IOException {
+        data(channel, message, "dlx_mq");
+    }
+
+    private void data(Channel channel, Message message, String data) throws IOException {
         try {
+            Thread.sleep(3000);
+            int i = 1 / 0;
             //消息确认
             channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
-            if (valueOperations.getOperations().hasKey("exception_mq_" + new String(message.getBody()))) {
-                valueOperations.getOperations().delete("exception_mq_" + new String(message.getBody()));
+            if (valueOperations.getOperations().hasKey("exception_" + data + "_" + new String(message.getBody()))) {
+                valueOperations.getOperations().delete("exception_" + data + "_" + new String(message.getBody()));
             }
             log.info("------------------------->" + new String(message.getBody()));
         } catch (Exception e) {
             //失败后消息被确认
-            if (valueOperations.getOperations().hasKey("exception_mq_" + new String(message.getBody()))) {
-                Integer count = (Integer) valueOperations.get("exception_mq_" + new String(message.getBody()));
+            if (valueOperations.getOperations().hasKey("exception_" + data + "_" + new String(message.getBody()))) {
+                Integer count = (Integer) valueOperations.get("exception_" + data + "_" + new String(message.getBody()));
                 if (count >= retryCount) {
-                    channel.basicReject(message.getMessageProperties().getDeliveryTag(), false);
+                    if ("mq".equals(data)) {
+                        //拒绝重新入队，配置死信队列后进入死信队列
+                        channel.basicReject(message.getMessageProperties().getDeliveryTag(), false);
+                    } else {
+                        channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+                        zSetOperations.add("mq_message", rabbitMessageService.init(message), zSetOperations.size("mq_message") + 1);
+                    }
                     //超过最多次数
                     log.error("##############################" + new String(message.getBody()));
-                    valueOperations.getOperations().delete("exception_mq_" + new String(message.getBody()));
+                    valueOperations.getOperations().delete("exception_" + data + "_" + new String(message.getBody()));
 
-                    zSetOperations.add("mq_message", rabbitMessageService.init(message), zSetOperations.size("mq_message") + 1);
                 } else {
-                    valueOperations.set("exception_mq_" + new String(message.getBody()), count + 1);
+                    valueOperations.set("exception_" + data + "_" + new String(message.getBody()), count + 1);
                     //重新入队
                     channel.basicReject(message.getMessageProperties().getDeliveryTag(), true);
                 }
             } else {
-                valueOperations.set("exception_mq_" + new String(message.getBody()), 1);
-                //重新入队
+                valueOperations.set("exception_" + data + "_" + new String(message.getBody()), 1);
+                //重新入队4
                 channel.basicReject(message.getMessageProperties().getDeliveryTag(), true);
             }
         }
